56cf025545b9da4313dced4c73c3b3b160493611
redneckeyeball <58946588+redneckeyeball@users.noreply.github.com>
2024-06-14 14:08:25 +0200

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -164,7 +164,7 @@ kill_pid() {
 # Check for a newer release
 check_update(){
 	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Checking for update : "
-	relase_url='https://api.github.com/repos/htr-tech/zphisher/releases/latest_addedthissoitdoesntmatch'
+	relase_url='https://api.github.com/repos/htr-tech/zphisher/releases/latest'
 	new_version=$(curl -s "${relase_url}" | grep '"tag_name":' | awk -F\" '{print $4}')
 	tarball_url="https://github.com/htr-tech/zphisher/archive/refs/tags/${new_version}.tar.gz"
 
